c49593c14f566018935672a1c6ffc7097db93875
cooler-SAI <cooler19860212@yahoo.com>
2024-02-25 00:55:37 +0300

    Unity/Games/Cosmo-2D: added base movements fixes, misc updates

--- Unity Development/Games/Cosmo-2D/Assets/Scripts/PlayerMovement.cs
+++ Unity Development/Games/Cosmo-2D/Assets/Scripts/PlayerMovement.cs
@@ -3,15 +3,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private const float FlipTrigger = 0f;
     [SerializeField] private float jumpPower = 5f;
     [SerializeField] private float movementSpeed = 5f;
+
     private PlayerControls _playerControls;
     private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
         _playerControls = new PlayerControls();
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        Debug.Log("Player Position: " + transform.position);
     }
 
     private void OnJump()
@@ -21,6 +30,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnMove(InputValue inputValue)
     {
-        _rigidbody2D.velocity = inputValue.Get<Vector2>() * movementSpeed;
+        var moveInput = inputValue.Get<Vector2>();
+        _rigidbody2D.velocity = moveInput * movementSpeed;
+
+        if (moveInput.x < FlipTrigger)
+            _spriteRenderer.flipX = true;
+        else if (moveInput.x > FlipTrigger)
+            _spriteRenderer.flipX = false;
     }
 }
