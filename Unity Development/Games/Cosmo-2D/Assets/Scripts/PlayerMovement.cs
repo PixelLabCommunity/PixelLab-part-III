a84492619d4200edd55d3065fa9a05d1f25984b5
cooler-SAI <cooler19860212@yahoo.com>
2024-02-26 21:45:40 +0300

    Unity/Games/Cosmo-2D: added physics materials, added double jump, added misc fixes

--- Unity Development/Games/Cosmo-2D/Assets/Scripts/PlayerMovement.cs
+++ Unity Development/Games/Cosmo-2D/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,11 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private const float FlipTrigger = 0f;
+    private const int MaxJumps = 2;
     [SerializeField] private float jumpPower = 5f;
     [SerializeField] private float movementSpeed = 5f;
+    private int _jumpCount;
+
 
     private PlayerControls _playerControls;
     private Rigidbody2D _rigidbody2D;
@@ -25,7 +28,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnJump()
     {
+        if (IsGrounded()) _jumpCount = 0;
+
+        if (_jumpCount >= MaxJumps) return;
+        _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 0f);
         _rigidbody2D.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        _jumpCount++;
     }
 
     private void OnMove(InputValue inputValue)
@@ -33,9 +41,16 @@ public class PlayerMovement : MonoBehaviour
         var moveInput = inputValue.Get<Vector2>();
         _rigidbody2D.velocity = moveInput * movementSpeed;
 
-        if (moveInput.x < FlipTrigger)
-            _spriteRenderer.flipX = true;
-        else if (moveInput.x > FlipTrigger)
-            _spriteRenderer.flipX = false;
+        _spriteRenderer.flipX = moveInput.x switch
+        {
+            < FlipTrigger => true,
+            > FlipTrigger => false,
+            _ => _spriteRenderer.flipX
+        };
+    }
+
+    private bool IsGrounded()
+    {
+        return Mathf.Abs(_rigidbody2D.velocity.y) < 0.01f;
     }
 }
