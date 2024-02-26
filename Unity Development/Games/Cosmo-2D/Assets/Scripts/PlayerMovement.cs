a2d1dd3f544b0a3c87964697ad4ec96b9a511005
cooler-SAI <cooler19860212@yahoo.com>
2024-02-26 22:34:02 +0300

    Unity/Games/Cosmo-2D: edit code, clearn up

--- Unity Development/Games/Cosmo-2D/Assets/Scripts/PlayerMovement.cs
+++ Unity Development/Games/Cosmo-2D/Assets/Scripts/PlayerMovement.cs
@@ -10,13 +10,11 @@ public class PlayerMovement : MonoBehaviour
     private int _jumpCount;
 
 
-    private PlayerControls _playerControls;
     private Rigidbody2D _rigidbody2D;
     private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
-        _playerControls = new PlayerControls();
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
