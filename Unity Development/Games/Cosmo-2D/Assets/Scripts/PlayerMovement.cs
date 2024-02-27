a482c74c404517b7bf7c23c42bfc2828b78897e1
cooler-SAI <cooler19860212@yahoo.com>
2024-02-27 22:05:57 +0300

    Unity/Games/Cosmo-2D: added new animations, misc fixes

--- Unity Development/Games/Cosmo-2D/Assets/Scripts/PlayerMovement.cs
+++ Unity Development/Games/Cosmo-2D/Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,12 @@ public class PlayerMovement : MonoBehaviour
     private const float PositionYZero = 0f;
     private const float GravityBase = 0.01f;
     private const int MaxJumps = 2;
+    private const string PlayerRun = "Run";
+    private static readonly int Run = Animator.StringToHash(PlayerRun);
     [SerializeField] private float jumpPower = 5f;
     [SerializeField] private float movementSpeed = 5f;
     private int _jumpCount;
+    private Animator _playerAnimator;
 
 
     private Rigidbody2D _rigidbody2D;
@@ -19,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _playerAnimator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -47,6 +51,11 @@ public class PlayerMovement : MonoBehaviour
             > FlipTrigger => false,
             _ => _spriteRenderer.flipX
         };
+
+        var isMoving =
+            moveInput.magnitude > 0; 
+
+        _playerAnimator.SetBool(Run, isMoving);
     }
 
     private bool IsGrounded()
