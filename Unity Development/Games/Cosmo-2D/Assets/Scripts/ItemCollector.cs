87a2c9ccaeb71ccb7a8dbfd14e1019b438fd4ba6
cooler-SAI <cooler19860212@yahoo.com>
2024-03-01 22:50:37 +0300

    Unity/Games/Cosmo-2D: update build, added new UI, misc up fixes, clearn up

--- Unity Development/Games/Cosmo-2D/Assets/Scripts/ItemCollector.cs
+++ Unity Development/Games/Cosmo-2D/Assets/Scripts/ItemCollector.cs
@@ -1,14 +1,21 @@
+using TMPro;
 using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
     private const string CollectableItem = "Fruit";
+    [SerializeField] private TextMeshProUGUI fruitCount;
+
     private int _count;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag(CollectableItem)) Destroy(other.gameObject);
-        _count++;
-        Debug.LogWarning("Collected: " + _count);
+        if (other.gameObject.CompareTag(CollectableItem))
+        {
+            Destroy(other.gameObject);
+            _count++;
+        }
+
+        fruitCount.text = "Fruits:" + _count;
     }
 }
