29a73b9509d8f195b59e3ee8dd22f4121ddb8d48
cooler-SAI <cooler19860212@yahoo.com>
2024-03-03 00:23:21 +0300

    Unity/Games/Cosmo-2D: set up restart level

--- Unity Development/Games/Cosmo-2D/Assets/Scripts/PlayerLife.cs
+++ Unity Development/Games/Cosmo-2D/Assets/Scripts/PlayerLife.cs
@@ -1,32 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
     private const string DamageDeal = "DamageDeal";
+    private const string FallCollider = "FallCollider";
     private static readonly int Death = Animator.StringToHash("death");
     private Animator _animator;
-    private Rigidbody2D _rigidbody2D;
+    private PlayerInput _playerInput;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
-        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _playerInput = GetComponent<PlayerInput>();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag(DamageDeal)) PlayerDie();
+        if (other.gameObject.CompareTag(DamageDeal) || other.gameObject.CompareTag(FallCollider)) PlayerDie();
     }
 
     private void PlayerDie()
     {
-        _rigidbody2D.bodyType = RigidbodyType2D.Static;
+        _playerInput.enabled = false;
         _animator.SetTrigger(Death);
     }
 
     private void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        _playerInput.enabled = true;
     }
 }
