57230f5ed813c3982200a3da97edc956766cf4e8
cooler-SAI <cooler19860212@yahoo.com>
2024-03-05 22:29:04 +0300

    Unity/Games/Cosmo-2D: added base script for rotation

--- Unity Development/Games/Cosmo-2D/Assets/Scripts/GameObjectRotation.cs
+++ Unity Development/Games/Cosmo-2D/Assets/Scripts/GameObjectRotation.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class GameObjectRotation : MonoBehaviour
 {
     private const int BaseRotation = 360;
+    private const int BaseValue = 0;
     [SerializeField] private float rotationSpeed = 2f;
 
 
     private void Update()
     {
-        transform.Rotate(0, 0, BaseRotation * rotationSpeed * Time.deltaTime);
+        transform.Rotate(BaseValue, BaseValue, BaseRotation * rotationSpeed * Time.deltaTime);
     }
 }
