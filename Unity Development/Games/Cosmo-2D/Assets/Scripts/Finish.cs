070429993c8c0bf7de6036f496d4aa4cb69636fd
cooler-SAI <cooler19860212@yahoo.com>
2024-03-09 21:52:48 +0300

    Unity/Games/Cosmo-2D: added level 3

--- Unity Development/Games/Cosmo-2D/Assets/Scripts/Finish.cs
+++ Unity Development/Games/Cosmo-2D/Assets/Scripts/Finish.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
+    private const float DelayTimer = 5f;
     private static readonly int FinishTouched = Animator.StringToHash("finishTouched");
     private static readonly int FinishedPlayer = Animator.StringToHash("finishedPlayer");
     public bool finished;
     private Animator _animator;
+    private bool _playFinished; // Added variable to track if play has finished
     public static Finish Instance { get; private set; }
-
 
     private void Awake()
     {
@@ -15,19 +17,21 @@ public class Finish : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            _animator.SetTrigger(FinishTouched);
-            _animator.SetTrigger(FinishedPlayer);
-        }
+        if (!other.gameObject.CompareTag("Player") || finished || _playFinished) return;
 
+        _animator.SetTrigger(FinishTouched);
+        _animator.SetTrigger(FinishedPlayer);
         finished = true;
+
+        if (_playFinished) return;
+        _playFinished = true;
+        Invoke(nameof(LevelComplete), DelayTimer);
     }
 
-    public void LevelComplete()
+    private void LevelComplete()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
