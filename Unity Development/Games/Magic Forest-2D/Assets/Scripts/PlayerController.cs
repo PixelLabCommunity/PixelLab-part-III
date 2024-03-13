b51219de3b8cd59035a34b3ddf27705526c28628
cooler-SAI <cooler19860212@yahoo.com>
2024-03-13 20:23:22 +0300

    Unity/Games/Magic Forest-2D: added player controls, misc clearn up fixes

--- Unity Development/Games/Magic Forest-2D/Assets/Scripts/PlayerController.cs
+++ Unity Development/Games/Magic Forest-2D/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float playerSpeed = 4f;
+    private Vector2 _movement;
+
+    private PlayerControls _playerControls;
+    private Rigidbody2D _playerRigidbody2D;
+
+    private void Awake()
     {
-        
+        _playerControls = new PlayerControls();
+        _playerRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void PlayerInput()
+    {
+        _movement = _playerControls.Movement.Move.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        _playerRigidbody2D.MovePosition(_playerRigidbody2D.position + _movement * (playerSpeed * Time.deltaTime));
     }
 }
