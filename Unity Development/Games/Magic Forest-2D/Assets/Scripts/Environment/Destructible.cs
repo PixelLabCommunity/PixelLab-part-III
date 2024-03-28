c443349a9ddd9c31082945f021b2922d53ef4d68
cooler-SAI <cooler19860212@yahoo.com>
2024-03-28 13:22:23 +0300

    Unity/Games/Magic Forest-2D: added new prefabs, misc updates

---
+++ Unity Development/Games/Magic Forest-2D/Assets/Scripts/Environment/Destructible.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    [SerializeField] private GameObject destroyVFX;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Damage")) Instantiate(destroyVFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
