916e068adc95158c0abf6d81e513595346dd4a23
cooler-SAI <cooler19860212@yahoo.com>
2024-05-16 22:35:10 +0300

    Unity/Games/Magic Forest-2D: add stuff attack

--- Unity Development/Games/Magic Forest-2D/Assets/Scripts/Weapon/Staff/MagicLaser.cs
+++ Unity Development/Games/Magic Forest-2D/Assets/Scripts/Weapon/Staff/MagicLaser.cs
@@ -2,4 +2,41 @@ using UnityEngine;
 
 public class MagicLaser : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 22f;
+    [SerializeField] private float destroyDelay = 1.5f;
+    [SerializeField] private GameObject destroyVFX;
+    [SerializeField] private float vfxDestroyDelay = 1f;
+
+    [SerializeField] private int arrowDamage = 1;
+
+    private void Start()
+    {
+        Destroy(gameObject, destroyDelay);
+    }
+
+    private void Update()
+    {
+        MoveLaser();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            var enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+            enemyHealth.Damage(arrowDamage);
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.CompareTag("UnDestructible"))
+        {
+            var vfxInstance = Instantiate(destroyVFX, transform.position, Quaternion.identity);
+            Destroy(vfxInstance, vfxDestroyDelay);
+            Destroy(gameObject);
+        }
+    }
+
+    private void MoveLaser()
+    {
+        transform.Translate(Vector3.right * (Time.deltaTime * moveSpeed));
+    }
 }
