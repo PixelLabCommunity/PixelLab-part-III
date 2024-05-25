81b6edd703644042aa2c91502e653ae345b5b520
cooler-SAI <cooler19860212@yahoo.com>
2024-05-25 15:23:03 +0300

    Unity/Games/Magic Forest-2D: update script

--- Unity Development/Games/Magic Forest-2D/Assets/Scripts/Weapon/Bow/Bow.cs
+++ Unity Development/Games/Magic Forest-2D/Assets/Scripts/Weapon/Bow/Bow.cs
@@ -32,7 +32,7 @@ public class Bow : MonoBehaviour, IWeapon
         _originalLocalPosition = transform.localPosition;
 
         // Ensure the bow is correctly oriented upon spawning
-        UpdateBowOrientation();
+        UpdateBowOrientation(true);
     }
 
     private void Start()
@@ -82,8 +82,7 @@ public class Bow : MonoBehaviour, IWeapon
     {
         var mousePosition = Input.mousePosition;
         if (Camera.main == null) return;
-        var worldMousePosition =
-            Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, transform.position.z));
+        var worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, transform.position.z));
         var direction = worldMousePosition - transform.position;
 
         var spawnPosition = arrowSpawnPoint.position;
@@ -118,48 +117,49 @@ public class Bow : MonoBehaviour, IWeapon
         if (_playerController.FacingLeft)
         {
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 180));
-            transform.localPosition = new Vector3(-Mathf.Abs(_originalLocalPosition.x), _originalLocalPosition.y,
-                _originalLocalPosition.z);
+            transform.localPosition = new Vector3(-Mathf.Abs(_originalLocalPosition.x), _originalLocalPosition.y, _originalLocalPosition.z);
             _spriteRenderer.flipY = true;
             _bowAnimator.SetBool(_lookLeftHash, true);
         }
         else
         {
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-            transform.localPosition = new Vector3(Mathf.Abs(_originalLocalPosition.x), _originalLocalPosition.y,
-                _originalLocalPosition.z);
+            transform.localPosition = new Vector3(Mathf.Abs(_originalLocalPosition.x), _originalLocalPosition.y, _originalLocalPosition.z);
             _spriteRenderer.flipY = false;
             _bowAnimator.SetBool(_lookLeftHash, false);
         }
     }
 
-    private void UpdateBowOrientation()
+    private void UpdateBowOrientation(bool forceImmediate = false)
     {
         _spriteRenderer.flipY = _playerController.FacingLeft;
         // Adjust the bow's local position based on the facing direction
         if (_playerController.FacingLeft)
         {
-            transform.localPosition = new Vector3(-Mathf.Abs(_originalLocalPosition.x), _originalLocalPosition.y,
-                _originalLocalPosition.z);
+            transform.localPosition = new Vector3(-Mathf.Abs(_originalLocalPosition.x), _originalLocalPosition.y, _originalLocalPosition.z);
             _bowAnimator.SetBool(_lookLeftHash, true);
         }
         else
         {
-            transform.localPosition = new Vector3(Mathf.Abs(_originalLocalPosition.x), _originalLocalPosition.y,
-                _originalLocalPosition.z);
+            transform.localPosition = new Vector3(Mathf.Abs(_originalLocalPosition.x), _originalLocalPosition.y, _originalLocalPosition.z);
             _bowAnimator.SetBool(_lookLeftHash, false);
+        }
+
+        if (forceImmediate)
+        {
+            transform.localPosition = _playerController.FacingLeft 
+                ? new Vector3(-Mathf.Abs(_originalLocalPosition.x), _originalLocalPosition.y, _originalLocalPosition.z)
+                : new Vector3(Mathf.Abs(_originalLocalPosition.x), _originalLocalPosition.y, _originalLocalPosition.z);
         }
     }
 
     private void DebugLog()
     {
         if (_activeWeapon == null)
-            Debug.LogError(
-                "ActiveWeapon reference not found! Make sure to assign it in the Unity Editor or set it through code.");
+            Debug.LogError("ActiveWeapon reference not found! Make sure to assign it in the Unity Editor or set it through code.");
 
         if (_playerController == null)
-            Debug.LogError(
-                "PlayerController reference not found! Make sure to assign it in the Unity Editor or set it through code.");
+            Debug.LogError("PlayerController reference not found! Make sure to assign it in the Unity Editor or set it through code.");
 
         if (_bowAnimator == null)
             Debug.LogError("Animator component is null! Ensure it is assigned and not destroyed.");
